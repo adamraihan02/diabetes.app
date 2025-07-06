2c28ce4e4be48641a63839908488b2567c36fe44
adamraihan02 <92570093+adamraihan02@users.noreply.github.com>
2025-07-06 14:05:49 +0700

    Update rumahsakit.py

--- components/rumahsakit.py
+++ components/rumahsakit.py
@@ -8,17 +8,18 @@ def run():
 
     # Data rumah sakit
     rumah_sakit = [
-        {"name": "Klinik Rumah Sakit Nusantara", "latitude": -6.2396683, "longitude": 106.6953988},
         {"name": "Bethsaida Hospital Diabetes Center", "latitude": -6.2548846, "longitude": 106.622344},
-        {"name": "Diabetes Connection Care Eka Hospital", "latitude": -6.2982839, "longitude": 106.6668542},
-        {"name": "Gading Diabetes Center - RS Gading Pluit", "latitude": -6.1660564, "longitude": 106.9133921},
-        {"name": "RUMAT Bogor - Perawatan Luka Diabetes", "latitude": -6.5295273, "longitude": 106.7074305},
-        {"name": "Klinik Kaki Diabetes - RS Puri Cinere", "latitude": -6.3218587, "longitude": 106.780195},
-        {"name": "RUMAT Depok - Perawatan Luka Diabetes", "latitude": -6.3216762, "longitude": 106.7003667},
-        {"name": "RUMAT Cibinong - Perawatan Luka Diabetes", "latitude": -6.4726847, "longitude": 106.6989847},
-        {"name": "RUMAT Cibubur - Perawatan Luka Diabetes", "latitude": -6.35199, "longitude": 106.7414191},
-        {"name": "Klinik Utama DR. Indrajana", "latitude": -6.176622, "longitude": 106.8145258},
-        {"name": "Rumah Sakit Mitra Keluarga Bintaro", "latitude": -6.2711177, "longitude": 106.7384151},
+        {"name": "Diabetes Connection Care Eka Hospital", "latitude": -6.2989036, "longitude": 106.6699139},
+        {"name": "Epitel Spesialis Luka Diabetes - Kelapa Gading Jakarta Utara", "latitude": -6.1723082, "longitude": 106.8987443},
+        {"name": "RUMAT Bogor - Perawatan Luka Diabetes", "latitude": -6.58685, "longitude": 106.7835865},
+        {"name": "Klinik Kaki Diabetes - RS Puri Cinere", "latitude": -6.3221637, "longitude": 106.7829289},
+        {"name": "RUMAT Depok - Perawatan Luka Diabetes", "latitude":-6.403715, "longitude": 106.8191169},
+        {"name": "RUMAT Cibinong - Perawatan Luka Diabetes", "latitude": -6.4726668, "longitude": 106.8432037},
+        {"name": "RUMAT Nanggewer - Perawatan Luka Diabetes", "latitude": -6.5192266, "longitude": 106.835354},
+        {"name": "RUMAT Cibubur - Perawatan Luka Diabetes", "latitude": -6.3519663, "longitude": 106.8855931},
+        {"name": "Klinik Diabetes Endokrin Metabolik - DR. Indrajana", "latitude": -6.1124109, "longitude": 106.7530914},
+        {"name": "Rumah Sakit Mitra Keluarga Bintaro", "latitude": -6.249699, "longitude": 106.6099367},
+        {"name": "Klinik Diabetes mGanik Care - Spesialis Diabetes", "latitude": -6.1904549, "longitude": 106.7032886},
         {"name": "Klinik AP & AP Pediatric, Growth and Diabetes Center", "latitude": -6.2283069, "longitude": 1106.8253455},
     ]
 
@@ -44,3 +45,4 @@ def run():
     # Tampilkan peta dalam Streamlit
     st.title("Peta Lokasi Rumah Sakit di Indonesia")
     st_folium(m, width=800, height=600)
+
